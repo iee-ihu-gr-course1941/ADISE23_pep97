4339d56497cb5ae9dcd91e415d8289153756e879
Konstantinos Piperis <kostaspiperis@gmail.com>
2024-01-06 14:47:18 +0200

    add board status route

---
+++ game/board-status.php
@@ -0,0 +1,46 @@
+<?php
+
+require_once "../lib/db_connect.php";
+require_once "battleship.php";
+
+session_start();
+header('Content-type: application/json');
+$response = [
+    'status' => 'error',
+    'data' => '',
+];
+
+if (!array_key_exists('user', $_SESSION)) {
+    $response['data'] = 'Not authenticated';
+} else if ($_SERVER['REQUEST_METHOD'] != 'GET') {
+    $response['status'] = 'error';
+    $response['data'] = 'Not supported method';
+} else {
+
+    $game = getActiveGame($mysqli, $_SESSION['user']['id']);
+    if (!$game) {
+        $response['status'] = 'error';
+        $response['data'] = 'There is not an active game';
+    } else {
+        $response['status'] = 'ok';
+
+        $placements = getShipPlacements($mysqli, $_SESSION['user']['id']);
+        $own_placements = [];
+
+        foreach ($placements as $placement) {
+            if ($placement['player'] == $_SESSION['user']['id']) {
+                $own_placements []= $placement;
+            }
+        }
+
+        $response['data'] = [
+            'ship_placements' => $own_placements,
+            'round_actions' => getRoundActions($mysqli, $_SESSION['user']['id']),
+            'game' => getActiveGame($mysqli, $_SESSION['user']['id']),
+        ];
+    }
+}
+
+$mysqli->close();
+print(json_encode($response, JSON_PRETTY_PRINT));
+?>
