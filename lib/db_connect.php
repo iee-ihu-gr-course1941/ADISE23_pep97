d2d74913b0d17488139ab927ad3e4b20373d6814
Konstantinos <kostaspiperis@gmail.com>
2024-01-08 20:38:39 +0200

    update configuration

--- lib/db_connect.php
+++ lib/db_connect.php
@@ -9,7 +9,7 @@ $db=$DB_NAME;
 
 
 if(gethostname()=='users.iee.ihu.gr') {
-    $mysqli = new mysqli($host, $user, $pass, $db,null,'/home/staff/asidirop/mysql/run/mysql.sock');
+    $mysqli = new mysqli($host, $user, $pass, $db,null,'/home/student/it/2015/it154522/mysql/run/mysql.sock');
 } else {
     $mysqli = new mysqli($host, $user, $pass, $db);
 }
